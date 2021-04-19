72434052ee648836f5414ec2a6873bc78cb27e4b
kylemhansen <kylehansen75@gmail.com>
2021-04-20 01:29:19 +0200

    LOGIN UPDATES

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <head>
 		<meta charset="UTF-8">
-		<title>Thriving Through Covid</title>
+		<title>BryteSA</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
 		<!-- google fonts -->
@@ -41,7 +41,7 @@ body, html {
 
 .stretch {
     width:100%;
-    height:935px;
+    /* height:100%; */
     z-index: -1; /* Ensure div tag stays behind content; -999 might work, too. */
 }
 .divInImage {
@@ -59,11 +59,12 @@ body, html {
 #bg a:hover {
     background-color:rgba(255,255,255,0.3);
 }
-#myImage_zone1{
-    width:32%;
-    height:14.5%;
-    top:640px;
-    left:39%;
+#banner{
+    object-fit: cover; /* Do not scale the image */
+  object-position: center; /* Center the image within the element */
+    width:100%;
+    height:50%;
+    position: relative;
 }
 #myImage_zone2{
     width:11%;
@@ -77,6 +78,12 @@ body, html {
     top:35%;
     left:52%;
 }
+.signup-form{
+    margin-top: 4%;
+    display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .btn-circle {
 				width: 70px;
 				height: 70px;
@@ -88,36 +95,39 @@ body, html {
 
             input, select {
                 height: 34px;
+                /* width: 30% !important; */
   display: block;
-  margin-bottom:42px;
-  margin-left:5%;
+  /* margin-bottom:42px; */
+  /* margin-left:5%; */
   padding: 1.6vh 1.4vh;
   box-sizing: border-box;
   font-family: 'Montserrat';
   font-weight: 500;
   font-size: 13px; }
-  input:focus, select:focus {
-    outline: none !important;
+
+  #regNowBtn{
+      position: relative;
+    left: 18%;
+    display: block;
+    width: 80%; 
+    height: 20%; 
 }
-input
-{
-    background: transparent !important;
-    border: none !important;
+
+.regBtn{
+    width: inherit !important;
+    height: inherit !important;
 }
 </style>
 
 </head>
 
 <body>
-
-<div id="bg">
-    <img src="{{ asset('img') }}/ttc/LOGIN PAGE8.jpg" class="stretch" alt="" />
-    <div id="myImage_zone1" class="divInImage">
-        <div class="signup-form">
+<div id="banner"><img src="{{ asset('img') }}/bryte/banner.jpg" class="stretch" alt="" /></div>
+<div class="signup-form">
             <form method="POST" class="login-form" id="login-form" action="{{ route('login') }}">
             @csrf
-                <div class="form-group row">
-                    <div class="col-md-6">
+                <div class="form-group">
+                <label for="email">{{ __('E-Mail Address') }}</label>
                         <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                         @error('email')
@@ -125,34 +135,47 @@ input
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
-                    </div>
                 </div>
-                <div class="form-group row">
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                
+
+                <div class="form-group">
+                <label for="password">{{ __('Password') }}</label>
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="test" required autocomplete="current-password">
 
                         @error('password')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
-                    </div>
                 </div>
+                <button type="button" onclick="login();" class="btn btn-success">{{ __('Login') }}</button>
 
-                <!-- <div class="form-group row mb-0">
-                    <div class="col-md-8 offset-md-4">
+                <div class="form-group row mb-1">
+                    <div class="col-md-8">
                         @if (Route::has('password.request'))
                             <a class="btn btn-link" href="{{ route('password.request') }}">
                                 {{ __('Forgot Your Password?') }}
                             </a>
                         @endif
                     </div>
-                </div> -->
+                </div>
+
             </form>
         </div>
-    </div>
-    <!-- <a id="myImage_zone2" onclick="event.preventDefault();alert('EVENT HAS NOT OPENED YET. PLEASE COME BACK ON THE 7TH TO LOGIN');"></a> -->
-    <a id="myImage_zone2" onclick="event.preventDefault();document.getElementById('login-form').submit();"></a>
-</div>
 
 </body>
+
+<script>
+
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+        window.location = "/mobile";
+}
+function login(){
+    var email = document.getElementById('email').value;
+    if(email != "wayne@flash-forward.co.za")
+        alert('THIS EVENT HAS NOT STARTED');
+    else
+        document.getElementById('login-form').submit();
+}
+</script>
