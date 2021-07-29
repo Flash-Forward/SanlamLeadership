5c47a703f40141867794122957d82db549121605
kylemhansen <kylehansen75@gmail.com>
2021-07-29 17:07:01 +0200

    COMMIT

--- resources/views/polls.blade.php
+++ resources/views/polls.blade.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-{{ PollWriter::draw(Inani\Larapoll\Poll::find(1)) }}
+{{ PollWriter::draw(Inani\Larapoll\Poll::find(2)) }}
 
 </body>
 </html>
