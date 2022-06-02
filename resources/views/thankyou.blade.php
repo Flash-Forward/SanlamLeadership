b4d85c41fec5409e3f0c406427b766b2babea144
kylemhansen <kylehansen75@gmail.com>
2022-06-02 10:39:53 +0200

    PUSH

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -2,7 +2,7 @@
 	<html lang="en">
 	<head>
 		<meta charset="UTF-8">
-    <title>SanlamConnect</title>
+    <title>Edulink Virtual Roadshow</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
 		<!-- google fonts -->
