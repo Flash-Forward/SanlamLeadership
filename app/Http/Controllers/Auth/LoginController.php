bfad1f2ef24e4b6a19dd2218068a5c97d8333ead
kylemhansen <kylehansen75@gmail.com>
2022-06-17 15:31:34 +0200

    PUSH

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -35,7 +35,7 @@ class LoginController extends Controller
             return '/lobby';
             break;
           case '2':
-            if(Auth::user()->email != "ffp@gmail.com" && Auth::user()->email != "edulink@gmail.com")
+            if(Auth::user()->email != "ffp@gmail.com" && Auth::user()->email != "sanlam@gmail.com")
               Auth::logout();
             return '/lobby';
             break; 
