f754daba185cc470674772ffadec8250ac689e54
kylemhansen <kylehansen75@gmail.com>
2024-05-17 08:09:14 +0200

    NAME

--- resources/views/auth/login3.blade.php
+++ resources/views/auth/login3.blade.php
@@ -245,7 +245,7 @@ s0.parentNode.insertBefore(s1,s0);
 
 <script>
             swalWithBootstrapButtons.fire({
-                        html: '<span style="color:#ffffff; font-size: 1.2vw;">Please note that the event will commence at 7 am on the 21st of June 2022</span>',
+                        html: '<span style="color:#ffffff; font-size: 1.2vw;">Please note that the event will commence on the 6th of June 2024</span>',
                         //html: '<span style="color:#ffffff; font-size: 1.2vw;">This room is closed</span>',
 
                         background: "url('{{ asset('img') }}/bryte/background.jpg')",
